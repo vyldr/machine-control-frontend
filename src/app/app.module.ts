c437092403de1868326202117b5476a3c207f5d4
wildesteven <wyld16steven@gmail.com>
2019-10-09 18:29:15 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ManageComponent } from './manage/manage.component';
+import { OperateComponent } from './operate/operate.component';
+import { AddComponent } from './add/add.component';
+
+const appRoutes: Routes = [
+  { path: 'signin',      component: SignInComponent  },
+  { path: '',            component: SignInComponent  },
+  { path: 'signup',      component: SignUpComponent  },
+  { path: 'manage',      component: ManageComponent  },
+  { path: 'add',         component: AddComponent     },
+  { path: 'operate/:id', component: OperateComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ManageComponent,
+    OperateComponent,
+    AddComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
